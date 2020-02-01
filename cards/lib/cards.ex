63d4bc2adbb68feb0321f08e7a0caaf084e0d066
Jonathan Dain Palacio <bangarangler@gmail.com>
2020-02-01 17:16:28 -0500

    init commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,34 @@
+defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # a list comprehension is essintally a map for every element in our suits array do something and put it into new array
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    #
+    # List.flatten(cards)
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+    # deck.shuffle
+  end
+
+end
